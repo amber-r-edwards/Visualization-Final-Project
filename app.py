dd3de2d1d500e279c5fc9a772f5ccaa316da3c67
amber-r-edwards <arose52400@gmail.com>
2025-11-21 10:00:32 -0500

    first push - basic set up of project and first it of home page

---
+++ app.py
@@ -0,0 +1,13 @@
+"""
+Flask App with Plotly.js and D3.js Visualizations
+==============================================
+
+This app reads text reuse data drawn from X feminist zines from XXXX-XXXX.
+It is meant as a research tool to understand knowledge sharing networks between local activists across the US.
+
+How to run:
+1. Install Flask: pip install -r requirements.txt
+2. Run: python app.py
+3. Open: http://localhost:5000
+"""
+
